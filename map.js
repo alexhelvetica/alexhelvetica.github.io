a6cdd5a5ae48d1a42f58c72a84c3835fe6f4366d
alexhelvetica <alex.s.helvetica+git@gmail.com>
2025-05-31 12:03:20 +1000

    removed ununeeded variable

--- map.js
+++ map.js
@@ -8,11 +8,10 @@ var wasteModifier = document.getElementById("wasteModifier");
 var wasteType = d3.select('input[name="wasteType"]:checked').node().value; 
 var mapView = document.getElementById("melbourne");
 
-var tempColor = ['#f2f0f7','#cbc9e2','#9e9ac8','#756bb1','#54278f'];
 var red = ['#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d'];
 var yellow = ['#FFFFF0','#FDFD96','#FFFDD0','#FFFF31','#FFFF00','#FCF75E','#FFF700','#FFEF00','#FBEC5D'];
 var green = ['#f7fcf5','#e5f5e0','#c7e9c0','#a1d99b','#74c476','#41ab5d','#238b45','#006d2c','#00441b'];
-var blue = tempColor;
+var blue = ['#f2f0f7','#cbc9e2','#9e9ac8','#756bb1','#54278f'];
 
 var geoPath;
 var projection;
