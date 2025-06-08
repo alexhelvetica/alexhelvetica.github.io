c1ca370f2874f4ec4296109034aa4ee063cd9e26
alexhelvetica <alex.s.helvetica+git@gmail.com>
2025-06-08 15:45:11 +1000

    line updated to d3 v7

--- js/line.js
+++ js/line.js
@@ -1,20 +1,26 @@
 import { width, height, padding, addSelectionHeading, removeSelectionHeading, getCommonName, createSvgCanvas, getCategoryColour } from "./common.js";
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@5/+esm";
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 var json;
 var svg;
 var xAxisScale;
+var yAxisScale;
+var wasteModifier = document.getElementById("lineWasteModifier");
 
 //Waste Per Capita vs Total Selector
-document.getElementById("lineWasteModifier")
-    .onchange = updateScale;
+wasteModifier.onchange = updateScale;
+
+const line =  d3.line()
+    .x((d) => xAxisScale(d.Reference_Year))
+    .y((d) => yAxisScale(scaleWasteByPopulation(d, false)));
 
 export async function initialiseLineChart() {
     await d3.csv("linev2.csv")
         .then(function (data) {
-            json = d3.nest()
-                .key((d) => d.Type)
-                .entries(data);
+            json = d3.group(
+                data,
+                (d) => d.Type
+            );
         });
 
     svg = createSvgCanvas(d3, "lineChart");
@@ -24,7 +30,7 @@ export async function initialiseLineChart() {
 
 function getXAxisScale() {
     var xAxisScale = d3.scaleLinear()
-        .domain(d3.extent(json[0].values, (d) => d.Reference_Year))
+        .domain(d3.extent(json.entries().next().value[1], (d) => d.Reference_Year))
         .range([padding, width]);
 
     var xAxias = d3.axisBottom()
@@ -39,9 +45,11 @@ function getXAxisScale() {
     return xAxisScale;
 }
 
-function getYAxisScale(event) {
+function getYAxisScale() {
     var yAxisScale = d3.scaleLinear()
-        .domain([0, d3.max(json, (d) => d3.max(d.values, (e) => scaleWasteByPopulation(e, event) * 1.5))])
+        .domain([0, d3.max(json.entries(), (d) => 
+            d3.max(d[1], (e) => scaleWasteByPopulation(e) * 1.5)
+        )])
         .range([height - padding, 0]);
 
     var yAxis = d3.axisLeft()
@@ -55,36 +63,32 @@ function getYAxisScale(event) {
     return yAxisScale;
 }
 
-function drawLineChart(event) {
-    var yAxisScale = getYAxisScale(event);
+function drawLineChart() {
+    yAxisScale = getYAxisScale();
     // Draw the line
     svg.selectAll(".line")
-        .data(json)
+        .data(json.entries())
         .enter()
         .append("path")
         .attr("class", "line")
-        .attr("stroke", (d) => getCategoryColour(d.key))
-        .attr("d", (d) =>
-            d3.line()
-                .x((d) => xAxisScale(d.Reference_Year))
-                .y((d) => yAxisScale(scaleWasteByPopulation(d, event)))
-                (d.values)
+        .attr("stroke", (d) => getCategoryColour(d[0]))
+        .attr("d", (d) => line(d[1])
         )
         .on("mouseover", function (event, d) {
-            addSelectionHeading(svg, getCommonName(json[d].key), json[d].values[0].Value);
+            addSelectionHeading(svg, getCommonName(d[0]), d[1][0].Value);
         })
         .on("mouseout", function (event, d) {
             removeSelectionHeading();
         })
         .append("title")
-        .text((d) => getCommonName(d.key));
+        .text((d) => getCommonName(d[0]));
 }
 
-function scaleWasteByPopulation(d, event) {
-    return +d.Value / (event?.target.checked ?? false ? +d.Estimated_Adult_Population : 1);
+function scaleWasteByPopulation(d) {
+    return +d.Value / (wasteModifier.checked ?? false ? +d.Estimated_Adult_Population : 1);
 }
 
 function updateScale(event) {
     svg.selectAll(".line,.yAxis").remove();
-    drawLineChart(event);
+    drawLineChart();
 }
